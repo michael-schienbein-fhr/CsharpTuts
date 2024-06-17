cd94bed4d4d0dfd0351c0ca2ef27f8a88023e7c7
Michael Schienbein <michael.schienbein@fhr.com>
2024-06-17 14:58:08 -0500

    stashersons

--- ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-internal class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
---
+++ ConsoleAppOne/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// Declare a string variable to hold the user's name
+string name;
+
+// Prompt the user to enter their name
+Console.WriteLine("What is your name?");
+
+// Read the user's input and store it in the 'name' variable
+name = Console.ReadLine();
+
+// Print a greeting message to the console, including the user's name
+Console.WriteLine($"Hello, {name}!");
